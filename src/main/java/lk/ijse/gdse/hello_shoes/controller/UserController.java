c3b4c4cd92f5a802fa9855ead5a975b6fd3b8c8f
Kaushalya13 <nimanthikaushalya13@gmail.com>
2024-04-22 11:27:06 +0530

    Add user

--- src/main/java/lk/ijse/gdse/hello_shoes/controller/UserController.java
+++ src/main/java/lk/ijse/gdse/hello_shoes/controller/UserController.java
@@ -1,17 +1,32 @@
 package lk.ijse.gdse.hello_shoes.controller;
 
+import lk.ijse.gdse.hello_shoes.reqAndrsp.response.JWTAuthResponse;
+import lk.ijse.gdse.hello_shoes.reqAndrsp.secure.SignIn;
+import lk.ijse.gdse.hello_shoes.reqAndrsp.secure.SignUp;
+import lk.ijse.gdse.hello_shoes.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
+    private final AuthenticationService authenticationService;
     @GetMapping("/health")
     public String healthCheck(){
         System.out.println("User Health Test");
         return "User Health Test";
+    }
+
+    //signUp
+    @PostMapping("signUp")
+    public ResponseEntity<JWTAuthResponse> signUp(@RequestBody SignUp signUp){
+        return ResponseEntity.ok(authenticationService.signUp(signUp));
+    }
+
+    //signIn
+    public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn){
+        return ResponseEntity.ok(authenticationService.signIn(signIn));
     }
 }
