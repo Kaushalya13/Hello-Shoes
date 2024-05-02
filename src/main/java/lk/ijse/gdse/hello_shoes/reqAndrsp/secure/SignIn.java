0ca3adaf8546a7ccd85f1a5bb3a48dd7bf0e7536
Kaushalya13 <nimanthikaushalya13@gmail.com>
2024-05-02 13:36:03 +0530

    updating

--- src/main/java/lk/ijse/gdse/hello_shoes/reqAndrsp/secure/SignIn.java
+++ src/main/java/lk/ijse/gdse/hello_shoes/reqAndrsp/secure/SignIn.java
@@ -1,11 +1,13 @@
 package lk.ijse.gdse.hello_shoes.reqAndrsp.secure;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
 public class SignIn {
     private String email;
