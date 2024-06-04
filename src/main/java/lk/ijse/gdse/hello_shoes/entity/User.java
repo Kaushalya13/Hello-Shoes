c4dbc5a9ba84c2012c22713b23b2fb630263a4e2
Kaushalya13 <nimanthikaushalya13@gmail.com>
2024-06-05 00:01:03 +0530

    User Updating

--- src/main/java/lk/ijse/gdse/hello_shoes/entity/User.java
+++ src/main/java/lk/ijse/gdse/hello_shoes/entity/User.java
@@ -19,8 +19,6 @@ import java.util.Set;
 @Data
 @Entity
 @Table(name = "user")
-@ToString(exclude = "sales")
-
 public class User implements UserDetails,SuperEntity{
     @Id
     private String userId;
